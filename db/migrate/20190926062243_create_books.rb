36737917f93d324635d9ff70c88f5fdf8333aaac
murata-naoya <murata.small.baseball@gmail.com>
2019-09-26 06:35:41 +0000

    userカラムとbooksモデルの作成

---
+++ db/migrate/20190926062243_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+      t.references :person, null: false, foreign_key: true
+      t.integer :favorites_count, default: 0
+      t.timestamps
+    end
+  end
+end
